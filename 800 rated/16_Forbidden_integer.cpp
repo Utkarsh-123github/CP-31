4e3a2e5240abd6876a3e34d35b9a44a1cba59104
Utkarsh Tiwari <utk0703@gmail.com>
2025-03-11 23:29:10 +0530

    CP-31 17

---
+++ 800 rated/16_Forbidden_integer.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k,x;
+        cin>>n>>k>>x;
+        if(x == 1 && k==1){
+            cout<<"NO"<<endl;
+        }
+        else if(x != 1){
+            cout<<"YES"<<endl;
+            cout<<n<<endl;
+            for(int i=0;i<n;i++){
+                cout<<1<<" ";
+            }
+            cout<<endl;
+        }
+        else{
+            if(n%2 == 0 && k>=2){
+                cout<<"YES"<<endl;
+                cout<<n/2<<endl;
+                for(int i=0;i<n/2;i++){
+                    cout<<2<<" ";
+                }
+                cout<<endl;
+            }
+            else if(n%2 != 0 && k==2){
+                cout<<"NO"<<endl;
+            }
+            else{
+                cout<<"YES"<<endl;
+                cout<<n/2<<endl;
+                for(int i=0;i<n/2-1;i++){
+                    cout<<2<<" ";
+                }
+                cout<<3<<endl;
+            }
+        }
+    }
+
+    return 0;
+}
